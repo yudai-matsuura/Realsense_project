d72dd84b33e2fda0d4fb5f13323d33451ae660b9
Matsuura Yudai <myudai0306@gmail.com>
2025-04-20 11:45:37 +0900

    modify code to pass build

--- terrain_analysis_pkg/include/terrain_analysis_pkg/terrain_roughness.hpp
+++ terrain_analysis_pkg/include/terrain_analysis_pkg/terrain_roughness.hpp
@@ -1,5 +1,5 @@
-#ifndef TERRAIN_ROUGHNESS_HPP_
-#define TERRAIN_ROUGHNESS_HPP_
+#ifndef TERRAIN_ANALYSIS_PKG__TERRAIN_ROUGHNESS_HPP_
+#define TERRAIN_ANALYSIS_PKG__TERRAIN_ROUGHNESS_HPP_
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
@@ -12,9 +12,15 @@
 #include <pcl/features/normal_3d.h>
 #include <pcl/search/kdtree.h>
 
+#include "terrain_analysis_pkg/visibility_control.h"
+
+namespace terrain_analysis_pkg
+{
+
 class TerrainRoughness : public rclcpp::Node
 {
 public:
+    TERRAIN_ROUGHNESS_PUBLIC
     TerrainRoughness();
 
 private:
@@ -22,5 +28,7 @@ private:
 
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud_sub_;
 };
+
+} // namespace terrain_analysis_pkg
 
 #endif // TERRAIN_ROUGHNESS_HPP_
