03d847a121ce7e92a38c440199cbbb8e1681423b
climbingroboticsteam <climbingroboticsteam@gmail.com>
2025-04-20 16:00:10 +0900

    fix launch file

--- lbr_terrain_analysis/launch/lbr_terrain_analysis.launch.py
+++ lbr_terrain_analysis/launch/lbr_terrain_analysis.launch.py
@@ -1,14 +1,13 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     terrain_analysis = Node(
-        package = 'terrain_analysis',
-        executable = 'terrain_analysis',
-        name = 'terrain_analysis',
-        output = 'screen'
+        package="terrain_analysis",
+        executable="terrain_analysis",
+        name="terrain_analysis",
+        output="screen"
     )
 
-    return LaunchDescription([
-        terrain_analysis
-    ])
+    return LaunchDescription(terrain_analysis)
