fc93e0849293840fa9a6e44a008e15780fb59956
Matsuura Yudai <myudai0306@gmail.com>
2025-04-24 16:45:35 +0900

    try to visualize normal

--- lbr_terrain_analysis/launch/lbr_terrain_analysis.launch.py
+++ lbr_terrain_analysis/launch/lbr_terrain_analysis.launch.py
@@ -8,6 +8,12 @@ def generate_launch_description():
             package="lbr_terrain_analysis",
             executable="lbr_terrain_analysis_node",
             name="lbr_terrain_analysis",
-            output="screen"
+            output="screen",
+        ),
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            output="screen",
         )
     ])
