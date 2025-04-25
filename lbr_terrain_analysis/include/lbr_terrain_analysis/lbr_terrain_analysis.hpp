7d2055af847bc70f72a026b96a81fd329175a4aa
climbingroboticsteam <climbingroboticsteam@gmail.com>
2025-04-25 12:48:07 +0900

    modify rviz setting

--- lbr_terrain_analysis/include/lbr_terrain_analysis/lbr_terrain_analysis.hpp
+++ lbr_terrain_analysis/include/lbr_terrain_analysis/lbr_terrain_analysis.hpp
@@ -53,6 +53,8 @@ private:
   void pointCloudCallback(
     const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
+    bool received_once = false;
+
   // Publisher
   rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
 
--- lbr_terrain_analysis/launch/lbr_terrain_analysis.launch.py
+++ lbr_terrain_analysis/launch/lbr_terrain_analysis.launch.py
@@ -1,8 +1,15 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
+    rviz_config_path = os.path.join(
+        get_package_share_directory('lbr_terrain_analysis'),
+        'config',
+        'terrain_analysis.rviz'
+    )
     return LaunchDescription([
         Node(
             package="lbr_terrain_analysis",
@@ -15,5 +22,6 @@ def generate_launch_description():
             executable="rviz2",
             name="rviz2",
             output="screen",
+            arguments=['-d', rviz_config_path],
         )
     ])
